0132871dd5c9506d806ba8764e0b2dd21480eabb
lvfvs <70900967+lvfvs@users.noreply.github.com>
2022-09-08 23:10:16 +1000

    Create Farmer class

--- src/Farmers.java
+++ src/Farmers.java
@@ -1,2 +1,14 @@
-public class Farmers {
+/*
+ *
+ *  File: Farmers.java
+ *  Assignment: Assignment 2
+ *  Course: COMP2240, Operating Systems
+ *  Author: Olivia Favos
+ *  Student Number: c3188124
+ *
+ */
+public class Farmers implements Runnable {
+    public void run() {
+
+    }
 }
