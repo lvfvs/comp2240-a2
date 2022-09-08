930115b45528f1e8e42d78a52212ad2960b08002
lvfvs <70900967+lvfvs@users.noreply.github.com>
2022-09-08 22:57:27 +1000

    Add comments

--- src/Bridge.java
+++ src/Bridge.java
@@ -10,23 +10,39 @@
 
 import java.util.concurrent.*;
 public class Bridge {
-    private final int BRIDGE_LENGTH = 20; // A farmer takes 20 steps to cross the bridge.
-    private final Semaphore bridge = new Semaphore(1);
-    private int neon = 0; // Tracks the number of farmers that have crossed the bridge.
+    private final int BRIDGE_LENGTH = 20; // A farmer takes 20 steps to cross the bridge
+    private final Semaphore bridge = new Semaphore(1); // A semaphore representing the bridge
+    private int neon = 0; // Tracks the number of farmers that have crossed the bridge
 
 
+    /**
+     * getBridgeLength method
+     * @return int value of the bridge length
+     */
     public int getBridgeLength() {
         return this.BRIDGE_LENGTH;
     }
 
+    /**
+     * getNeon method
+     * @return int value of the amount of times that farmers have crossed the bridge
+     */
     public int getNeon() {
         return this.neon;
     }
 
+    /**
+     * setNeon method
+     * @param neon
+     */
     public void setNeon(int neon) {
         this.neon = neon;
     }
 
+    /**
+     * incrementNeon method
+     * Increments the neon count
+     */
     public void incrementNeon() {
         this.neon++;
     }
